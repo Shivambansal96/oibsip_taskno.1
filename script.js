448d42564f488312f9e3b22d4372e1804c82ed12
ShivamBansal96 <bansal.shivam1216@gmail.com>
2024-03-01 11:38:04 +0530

    Task 1 Done

--- script.js
+++
@@ -1,7 +0,0 @@
-const dark = document.getElementsByClassName('dark');
-
-// dark.addEventListener
-dark.addEventListener("change", () => {
-    // Toggle website theme
-    document.body.classList.toggle("dark");
-  });
